c6d80b7497256e9668572b36690fc1757f8d5872
Fabio <fabio.ferreira@tecnofit.com.br>
2022-09-06 20:06:10 -0300

    second commit

--- lib/componens/horizontal_listview.dart
+++ lib/componens/horizontal_listview.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../home.dart';
+//testando uma commit
 
 class HorizontalList extends StatefulWidget {
   HorizontalList();
